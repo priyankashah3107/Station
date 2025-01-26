e0a68757f6e36b2ddd6f536e0e1e5e8d5c3115a4
Priyanka <priyankashah3107@gmail.com>
2025-01-26 22:19:45 +0530

    feat: finish

--- src/components/CreateShow.tsx
+++ src/components/CreateShow.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Image from "next/image";
+import Footer from "./Footer";
+import Button from "./Button";
 
 const logos = [
   {
@@ -105,6 +107,10 @@ const CreateShow = () => {
       <ClaimYourStation />
       <ShowCase />
       <Price />
+      <Testinomails />
+      <GetInFront />
+      <CreateShowForm />
+      <Footer />
     </>
   );
 };
@@ -343,8 +349,8 @@ function Price() {
   return (
     <>
       <section className="flex flex-col px-4 lg:p-24 py-16 gap-9 w-full bg-secondary-lite max-w-screen-2xl mx-auto">
-        <div className="flex flex-col gap-2">
-          <div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
             <h1 className="text-[28px] md:text-[40px] md:leading-[36px] font-semibold text-black tracking-tighter">
               Priced for all show sizes
             </h1>
@@ -353,20 +359,18 @@ function Price() {
             </p>
           </div>
           <div className="flex gap-3 w-full overflow-x-auto xl:overflow-x-visible no-scrollbar hover:scrollbar-thin items-start">
-            <div className="flex flex-row gap-4 overflow-x-auto scrollbar-hide pb-4 pt-2">
+            <div className="flex flex-row gap-4 overflow-x-auto scrollbar-hide pb-4 pt-2 cursor-pointer">
               {pricingData.map((plan, index) => (
                 <div
                   key={index}
-                  className={`border rounded-lg p-6 shadow-md flex flex-col justify-between  border-tertiary px-6 py-9 gap-6 shrink-0 overflow-hidden bg-white hover:bg-black hover:text-white w-60 h-[400px] ${
+                  className={`border rounded-lg p-6 shadow-md flex flex-col justify-between  border-tertiary px-6 py-9 gap-1 c shrink-0 overflow-hidden bg-white hover:bg-black hover:text-white w-60 h-auto ${
                     plan.title === "Indie" ? "bg-white" : "bg-white"
                   }`}
                 >
                   <h2 className="text-xl font-bold">{plan.title}</h2>
-                  <p className="text-2xl font-semibold mt-2">{plan.price}</p>
-                  <p className="text-sm text-gray-600 mt-1">
-                    {plan.emailLimit}
-                  </p>
-                  <ul className="mt-4 space-y-2">
+                  <p className="text-lg font-semibold ">{plan.price}</p>
+                  <p className="text-sm text-gray-600 ">{plan.emailLimit}</p>
+                  <ul className="mt-4 ">
                     {plan.features.map((feature, i) => (
                       <li key={i} className="flex items-center">
                         <span className="text-green-500 mr-2">✔</span>
@@ -380,6 +384,170 @@ function Price() {
           </div>
         </div>
       </section>
+    </>
+  );
+}
+
+function GetInFront() {
+  return (
+    <>
+      <section>
+        <div className="flex flex-col md:flex-row gap-12 lg:gap-24 items-center justify-around mt-32 md:mt-32 mb-6 ">
+          <div className="flex flex-col gap-3 lg:gap-6 items-center justify-center text-center">
+            <h1 className="text-[28px] md:text-[40px] leading-[120%] lg:leading-[40px] font-bold text-black tracking-tight lg:max-w-sm">
+              Ignite fandom and discussions.
+            </h1>
+            <p className="text-black max-w-sm font-medium text-[20px] leading-[20px]">
+              Consolidate your offers, discussions, subscriptions and more.
+            </p>
+          </div>
+          <div className="lg:w-[700px] lg:h-[500px] object-cover">
+            <Image
+              src={"/Get.png"}
+              alt="show"
+              width={500}
+              height={500}
+              className="lg:w-[625px] lg:h-[500px] "
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+function Testinomails() {
+  return (
+    <>
+      <div className="flex px-4 lg:p-24 py-16 items-center justify-center gap-16 bg-black">
+        <div className="flex flex-col lg:flex-row md:gap-16 gap-6 items-center">
+          <Image
+            alt="new heights logo"
+            loading="lazy"
+            width="202"
+            height="182"
+            decoding="async"
+            data-nimg="1"
+            className="w-full md:w-[202px]"
+            src="/podcast/p4.png"
+          />
+          <div className="flex flex-col gap-4 w-full max-w-[650px]">
+            <h2 className="text-[20px] font-semibold text-white text-center md:text-left leading-[120%]">
+              “Station has been a driving force behind helping us build and
+              engage the New Heights fan base this year.”
+            </h2>
+            <p className="md:text-lg text-white text-center md:text-left leading-[100%] font-medium">
+              Jake Perlmutter, Strategy &amp; Operations, Wave Sports +
+              Entertainment
+            </p>
+          </div>
+        </div>
+        <div className="hidden md:flex flex-col md:flex-row md:gap-16 gap-6 items-center border-l border-white/20 pl-16">
+          <div className="flex flex-col gap-4 w-full max-w-[650px]">
+            <h2 className="text-[20px] font-semibold text-white text-center md:text-left leading-[120%]">
+              The "Discord for Podcasts" bridges the gap between creators and
+              listeners, fostering deeper engagement and unlocking new
+              monetization opportunities.
+            </h2>
+            <p className="md:text-lg text-white text-center md:text-left leading-[100%] font-mediumx">
+              Yahoo Finance
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CreateShowForm() {
+  const [email, setEmail] = useState("");
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <form onSubmit={onFormSubmit} className="bg-[#F3F4F6] py-8">
+        <div className="flex justify-center mt-20 px-3 py-2 ">
+          <div className=" rounded-lg  p-4 w-full max-w-lg relative">
+            <div className="flex flex-col gap-2">
+              <h5 className="text-[28px] md:text-[40px] md:leading-[36px] font-bold text-text-primary tracking-tighter">
+                Claim your Station, it’s free!
+              </h5>
+              <p className="md:text-lg text-text-primary font-semibold">
+                Unlock your audience, and monetize faster.
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-y-4">
+              <label className="mt-4 text-sm font-semibold peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                Email address
+              </label>
+              <input
+                className="flex w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-800 sm:text-sm sm:leading-6 py-2.5 px-2"
+                id="email"
+                placeholder="example@example.com"
+                type="email"
+                value={email}
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <div>
+                <Button
+                  buttonName="Claim your station"
+                  color="w-full items-center justify-center whitespace-nowrap rounded-md transition-all duration-200 text-sm font-medium disabled:opacity-50 focus-visible:outline-none bg-gradient-to-r from-[#FE0307] to-[#D90AB9] text-white opacity-100 hover:opacity-90 py-2 px-4 shadow"
+                  size="px-6 py-2.5"
+                  className="rounded"
+                >
+                  Claim your Show
+                </Button>
+              </div>
+            </div>
+            {/* lines */}
+            <div className="relative flex justify-center items-center gap-4 text-sm my-2 text-[#707481]">
+              <div className="h-[1px] w-full rounded-lg bg-[#DEE0E4] undefined"></div>
+              or
+              <div className="h-[1px] w-full rounded-lg bg-[#DEE0E4] undefined"></div>
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md transition-all duration-200 text-sm font-medium disabled:opacity-50 focus-visible:outline-none bg-black text-white hover:bg-black focus:bg-primary-hover border border-primary h-10 px-7 py-2 w-full">
+                <svg
+                  className="h-5 w-5 mr-2"
+                  aria-hidden="true"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
+                    fill="#EA4335"
+                  ></path>
+                  <path
+                    d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
+                    fill="#4285F4"
+                  ></path>
+                  <path
+                    d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
+                    fill="#FBBC05"
+                  ></path>
+                  <path
+                    d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
+                    fill="#34A853"
+                  ></path>
+                </svg>
+                <span>Sign in with Google</span>
+              </button>
+              <p className="text-xs">
+                By clicking &#39;Enter Community&#39; or &#39;Continuing with
+                Google&#39; I agree to the Drop Station Inc.{" "}
+                <span className="font-semibold">Term of Services</span> and have
+                read the <span className="font-semibold">Privacy Policy.</span>{" "}
+                I also agree that Station may{" "}
+                <span className="font-semibold">share my information</span> with
+                Club 92.
+              </p>
+            </div>
+          </div>
+        </div>
+      </form>
     </>
   );
 }
